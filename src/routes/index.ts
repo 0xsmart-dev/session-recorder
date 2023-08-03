5df52c890417d56aabc43477630a0364fb2de0a3
smart-dev <vivitedyx99@gmail.com>
2023-08-03 11:43:58 -0400

    bugfix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,35 @@
-import { NextFunction, Response } from "express";
-import { createRecord } from "../utils/airtable.util";
-const recorderMiddleware = async (
-  req: any,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const sessionID = req.session.id;
-    const ipAddress = req.clientIp;
-    const urlParams = req.url;
-    const timestamp = new Date();
-    const data = {
-      session: sessionID,
-      ip: ipAddress,
-      url: JSON.stringify(urlParams),
-      timestamp: timestamp.toISOString(),
-    };
-    await createRecord("session", data);
-  } catch (error) {
-    res.status(500).send("Airtable Error: " + error);
-  }
-  next();
-};
-export default recorderMiddleware;
+import express from "express";
+import recorderMiddleware from "../middlewares/recorder.middleware";
+import apiController from "../controllers/api.controller";
+
+const router = express.Router();
+
+// API routes
+router.get("/api/records", apiController.getRecords);
+
+// Page routes
+router.get("/", recorderMiddleware, (req, res) => {
+  res.render("index");
+});
+
+router.get("/page1", recorderMiddleware, (req, res) => {
+  res.render("page1");
+});
+
+router.get("/page2", recorderMiddleware, (req, res) => {
+  res.render("page2");
+});
+
+router.get("/page3", recorderMiddleware, (req, res) => {
+  res.render("page3");
+});
+
+router.get("/page4", recorderMiddleware, (req, res) => {
+  res.render("page4");
+});
+
+router.get("/page5", recorderMiddleware, (req, res) => {
+  res.render("page5");
+});
+
+export default router;
